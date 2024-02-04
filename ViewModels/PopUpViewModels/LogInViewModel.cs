5bb4584c71ef36def7b6bc4e9154398c9d3e6fde
zoomerrremover@gmail.com <zoomerrremover@gmail.com>
2024-02-04 09:17:57 -0500

    Removed spawned garbage

---
+++ ViewModels/PopUpViewModels/LogInViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+using CommunityToolkit.Mvvm.Input;
+using TheJobOrganizationApp.ViewModels.Base;
+
+namespace TheJobOrganizationApp.ViewModels.PopUpViewModels;
+
+public partial class LogInViewModel: BaseViewModel
+{
+
+
+    [RelayCommand]
+    async Task goToScheldude()
+    {
+        await Shell.Current.GoToAsync("..");//{nameof(ScheldudePage)}
+    }
+}
